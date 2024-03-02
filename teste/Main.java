29b1614a180365d83074bbedfd60e4d69222d063
GuilhermeRomero <116127353+TheRomeroGuilherme@users.noreply.github.com>
2024-03-01 21:37:39 -0300

    trocando

--- teste/Main.java
+++ teste/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Pessoa p1 = new Pessoa(1, 2);
+        Pessoa p1 = new Pessoa(2021);
+        Funcionario f1 = new Funcionario(2021, 19, 78987);
         System.out.println("Pessoa 1: O RG e:" + p1.getRG() + " A idade e: " + p1.getIdade());
-        Pessoa p2 = new Pessoa(2);
+        Pessoa p2 = new Pessoa(2020);
+        Funcionario f2 = new Funcionario(2020, 17, 98779);
         System.out.println("Pessoa 2: O RG e:" + p2.getRG() + " A idade e: " + p2.getIdade());
     }
 }
